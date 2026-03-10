18c1079afb847d8ce147710d59dd55b0468589e8
CLB-coronelguasap <caleb.serranoobando@education.nsw.gov.au>
2026-03-10 11:52:42 +1100

    frfr

---
+++ training.py
@@ -0,0 +1,5 @@
+import random
+
+food = ['Crunchy Taco', 'Chipotle Chicken Burrito', 'Crunchwrap Supreme', 'Nachos BellGrande', 'Soft Taco', 'Chicken Quesadilla', 'Mexican Pizza']
+drinks = ['Pepsi', 'Diet Pepsi', 'Baja Blast', 'Mtn Dew', 'Strawberry Passionfruit Agua Refresca', 'Blue Raspberry Freeze']
+sides = ['Cinnabon Delights', '']
