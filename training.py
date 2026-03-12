857a7879aacbd2cc00a2d633239bb3c4403d9f3c
CLB-coronelguasap <caleb.serranoobando@education.nsw.gov.au>
2026-03-13 10:27:56 +1100

    update training.py

--- training.py
+++ training.py
@@ -6,7 +6,7 @@ import os
 food = ['Crunchy Taco', 'Chipotle Chicken Burrito', 'Crunchwrap Supreme', 'Nachos BellGrande', 'Soft Taco', 'Chicken Quesadilla', 'Mexican Pizza']
 drinks = ['Pepsi', 'Diet Pepsi', 'Baja Blast', 'Mtn Dew', 'Strawberry Passionfruit Agua Refresca', 'Blue Raspberry Freeze']
 sides = ['Cinnabon Delights', 'Fries', 'Beans and Rice', 'Beans and Cheese', 'Chips and Guac', 'Chips and Nacho Cheese']
-sizes = ['Small', 'Medium', 'Large']
+sizes = ['Small', 'Medium', 'Large', 'Extra Large']
 sauces = ['Verde Salsa', 'Mild', 'Hot', 'Fire', 'Diablo', 'Nacho Cheese', 'Sour Cream', 'Creamy Jalapeño', 'Chipotle', 'Guacamole', 'Avocado Ranch', 'Spicy Ranch', 'Red']
 
 print(str(random.randint(1,5)) + 'x ' + random.choice(food))
@@ -22,7 +22,10 @@ def start_training(root):
     game_frame.pack(expand=True)
     
     customer_img = tk.PhotoImage(file=f"{os.getcwd() + '\\assets\\images\\customer\\'}{random.choice(os.listdir(str(os.getcwd()) + "\\assets\\images\\customer\\"))}")
-    customer = tk.Label(game_frame, customer_img)
+    customer_img_subsampled = customer_img.subsample(2, 2)
+    customer = tk.Label(game_frame, image=customer_img_subsampled)
+    customer.image = customer_img_subsampled
+    customer.grid(row=0, column=0, sticky=tk.NW)
     customer.pack()
     
     
